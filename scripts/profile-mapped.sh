23d8a64fd2cc5df0477493440544e6ac207cbece
feargusog <feargusog@gmail.com>
2021-03-21 12:59:52 +0000

    update project structure

--- scripts/profile-mapped.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-valgrind --tool=massif -v ./bazel-bin/mmd-vector m
-
-for f in ./massif.out.*; do
-    ms_print $f > ./memory-profiles/"mapped-profile.txt"
-done
-
-rm ./massif.out.*
--- scripts/profile-standard.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-valgrind --tool=massif -v ./bazel-bin/mmd-vector s
-
-for f in ./massif.out.*; do
-    ms_print $f > ./memory-profiles/"mapped-standard.txt"
-done
-
-rm ./massif.out.*

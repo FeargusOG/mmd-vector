78819f360533b0226dd9ad9a611df2b82429c17b
feargusog <feargusog@gmail.com>
2021-04-05 13:01:59 +0100

    Improve unit testing

--- tests/mmd-vector-test.cc
+++ tests/mmd-vector-test.cc
@@ -27,31 +27,48 @@ namespace mmd
 
         TEST(MmdVector, ctor_copy)
         {
+            // Create a vector
             const double test_val = 2.0;
             MmdVector<double> orig_vector{10};
             EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 0);
+
+            // Push a value to it
             orig_vector.push_back(test_val);
             EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 1);
             EXPECT_DOUBLE_EQ(orig_vector.get_mapped_vector()->at(0), test_val);
 
+            // Copy it to a new vector
             MmdVector<double> copy_vector{orig_vector};
             EXPECT_EQ(copy_vector.get_mapped_vector()->size(), 1);
             EXPECT_DOUBLE_EQ(copy_vector.get_mapped_vector()->at(0), test_val);
+
+            // Make sure that the original vector is left untouched
+            EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 1);
+            EXPECT_DOUBLE_EQ(orig_vector.get_mapped_vector()->at(0), test_val);
+
         }
 
         TEST(MmdVector, ctor_move)
         {
+            // Create an empty vector
             const double test_val = 2.0;
             MmdVector<double> orig_vector{10};
             EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 0);
+
+            // Push a value to it
             orig_vector.push_back(test_val);
             EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 1);
             EXPECT_DOUBLE_EQ(orig_vector.get_mapped_vector()->at(0), test_val);
 
+            // Move the vector to a new one
             MmdVector<double> move_vector{std::move(orig_vector)};
-            EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 0);
             EXPECT_EQ(move_vector.get_mapped_vector()->size(), 1);
             EXPECT_DOUBLE_EQ(move_vector.get_mapped_vector()->at(0), test_val);
+
+            // Ensure the original vector has been 'reset'
+            EXPECT_EQ(orig_vector.get_mapped_vector()->size(), 0);
+            orig_vector.push_back(test_val);
+            EXPECT_DOUBLE_EQ(orig_vector.get_mapped_vector()->at(0), test_val);
         }
 
         TEST(MmdVector, swap)
@@ -73,11 +90,15 @@ namespace mmd
 
                 // Swap ownership
                 swap_vector.swap(new_vector);
-                EXPECT_EQ(new_vector.get_mapped_vector()->size(), 0);
                 EXPECT_EQ(swap_vector.get_mapped_vector()->size(), 1);
+
+                // Ensure the original vector has been 'reset'
+                EXPECT_EQ(new_vector.get_mapped_vector()->size(), 0);
+                new_vector.push_back(test_val);
+                EXPECT_DOUBLE_EQ(new_vector.get_mapped_vector()->at(0), test_val);
             }
 
-            // The other vector has gone out of scope. Lets see we do still own it.
+            // The other vector has gone out of scope. Lets see we do still own it's memory.
             EXPECT_EQ(swap_vector.get_mapped_vector()->size(), 1);
             EXPECT_DOUBLE_EQ(swap_vector.get_mapped_vector()->at(0), test_val);
         }
